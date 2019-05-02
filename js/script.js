f5824a999e3806d41abebbb7ff0a614ba3cda7e5
anisa-matthews <anisa.matthews11@gmail.com>
2019-05-02 16:35:55 -0400

    basics

--- js/script.js
+++ js/script.js
@@ -15,4 +15,10 @@ $(document).ready(function(){
         $("#monitor").show();
         setTimeout(function() { $("#monitor").hide(); }, 3000);
     });
+
+    //click pelvic scan
+    $("button").click(function(){
+        $(".svg").attr("src", "images/pelvic-scan.gif");
+        setTimeout(function() { $(".svg").attr("src", "images/smile.svg"); }, 3500);
+    });
 });
